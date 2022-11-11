c79ea9363284c4658d696a2f5e54245033ae7296
thanhtin4401 <thanhtin4401.ufm@gmail.com>
2022-11-11 10:59:17 +0700

    commit date 11

---
+++ Server/Routers/hotel.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+
+//CREATE
+router.post("/", (req, res) => {});
+//UPDATE
+//DELETE
+//GET
+//GET ALL
+//PUT
+module.exports = router;
---
+++ Server/models/Hotel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const HotelSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  addresss: {
+    type: Sting,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  photos: {
+    type: [String],
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  rooms: {
+    type: [String],
+  },
+  cheaperstPrice: {
+    type: String,
+    required: true,
+  },
+});

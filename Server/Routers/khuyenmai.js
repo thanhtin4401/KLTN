a9bfa150fcf385e762781c130dea514269ac9947
quangptgg <107898920+quangptgg@users.noreply.github.com>
2022-12-13 09:52:11 +0700

    CRUD KhuyenMai

--- Server/Routers/khuyenmai.js
+++ Server/Routers/khuyenmai.js
@@ -1,28 +1,27 @@
-// const khachSanController = require("../controllers/khachSanController");
-const middlewareController = require("../controllers/middlewareController");
+const middlewareController = require('../controllers/middlewareController')
+const khuyenMaiController = require('../controllers/khuyenMaiController')
 
-const khuyenMaiController = require("../controllers/khuyenMaiController");
+const router = require('express').Router()
 
-const router = require("express").Router();
+router.get('/', khuyenMaiController.getAllPromotions)
+router.get('/:id', khuyenMaiController.getPromotionById)
 
-//Create
-router.post("/api/khuyen-mai", khuyenMaiController.createPromotion);
-
-//GET ALL USERS
-router.get("/api/khuyen-mai", khuyenMaiController.getAllPromotion);
-
-//GET BY ID
-router.get("/api/khuyen-mai", khuyenMaiController.getPromotionById);
-
-// Update
-router.put("/api/khuyen-mai", khuyenMaiController.updatePromotion);
-
-//DELETE Promotion
-//v1/user/2313123
-router.delete(
-  "/api/khuyen-mai:id",
+router.post(
+  '/',
   middlewareController.verifyTokenAndAminAuth,
-  khuyenMaiController.deletePromotion
-);
+  khuyenMaiController.createPromotion,
+)
 
-module.exports = router;
+router.put(
+  '/:id',
+  middlewareController.verifyTokenAndAminAuth,
+  khuyenMaiController.updatePromotion,
+)
+
+router.delete(
+  '/:id',
+  middlewareController.verifyTokenAndAminAuth,
+  khuyenMaiController.deletePromotion,
+)
+
+module.exports = router
